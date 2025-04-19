24ecb4ef92bde21ae27615ca7698992d141796df
Pedro L. Esquinas <pedro.esquinasfernandez@bccancer.bc.ca>
2025-04-18 22:30:44 -0700

    First implementation of reconstruction script

--- reconstruction/Tools.py
+++ reconstruction/Tools.py
@@ -5,11 +5,10 @@ from pytomography.algorithms import OSEM
 from pytomography.projectors.SPECT import SPECTSystemMatrix
 from pytomography.likelihoods import PoissonLogLikelihood
 import pydicom
-from pydicom import FileDataset
 import torch
-
-from typing import List, Dict, Tuple
-
+from typing import List, Dict
+from Subsample import subsample_projections_number, subsample_projections_time
+from pytomography.utils.scatter import get_smoothed_scatter
 
 def reconstruct_study(projection_data_dcm: List[str], ct_data_dcm: List[str], params: Dict, output_path: str) -> None:
     """Reconstruct multi-bed SPECT projection data utilizing reconstruction parameters defined by the 
@@ -35,9 +34,7 @@ def reconstruct_study(projection_data_dcm: List[str], ct_data_dcm: List[str], pa
     # Load Projections
     projections = dicom.load_multibed_projections(projection_data_dcm)
     
-    # Confirm Energy Window Settings
-    proj_dcm = pydicom.dcmread(projection_data_dcm[0])
-    
+    # Confirm Energy Window Settings    
     for i, energy, win_name in zip([index_photopeak, index_lower, index_upper], [208, 180, 235], ["Peak", "Lower", "Upper"]):
         low, high = dicom.get_energy_window_bounds(projection_data_dcm[0], idx=i)
         if energy > high or energy < low:
@@ -47,12 +44,44 @@ def reconstruct_study(projection_data_dcm: List[str], ct_data_dcm: List[str], pa
     
     for bed_pos_proj, bed_pos_dcm in zip(projections, projection_data_dcm):
         
+        
+        
+        # Read original data
+        dicom_ds = pydicom.dcmread(bed_pos_dcm, force=True)
         object_meta, proj_meta = dicom.get_metadata(bed_pos_dcm, index_peak=index_photopeak)
         photopeak_projections = bed_pos_proj[index_photopeak]
-        scatter_projections = dicom.get_energy_window_scatter_estimate_projections(
-            file=bed_pos_dcm, projections=bed_pos_proj, index_peak=index_photopeak, index_lower=index_lower, index_upper=index_upper
+        upper_projections = bed_pos_proj[index_upper]
+        lower_projections = bed_pos_proj[index_lower]
+        
+        # Apply Projection Time Reduction (Poisson thinning):
+        if "Time" in params["Data_Reduction"]:
+            
+            photopeak_projections = subsample_projections_time(projections=photopeak_projections, t_reduction_factor=params["Data_Reduction"]["Time"])
+            upper_projections = subsample_projections_time(projections=upper_projections, t_reduction_factor=params["Data_Reduction"]["Time"])
+            lower_projections = subsample_projections_time(projections=lower_projections, t_reduction_factor=params["Data_Reduction"]["Time"])
+        
+        # Estimate Scatter using TEW
+        scatter_projections = dicom.compute_EW_scatter(
+            projection_lower=lower_projections,
+            projection_upper=upper_projections,
+            width_peak=dicom.get_window_width(dicom_ds, index_photopeak),
+            width_lower=dicom.get_window_width(dicom_ds, index_lower),
+            width_upper=dicom.get_window_width(dicom_ds, index_upper)
         )
         
+        if params["Data_Reduction"]["Smooth_Scatter"]:
+            scatter_projections = get_smoothed_scatter(scatter=scatter_projections, proj_meta=proj_meta, sigma_r=1.0, sigma_z=1.0)
+        
+        # Apply data reduction: subsample projections number
+        if "Projections" in params["Data_Reduction"]:
+            photopeak_projections, scatter_projections, object_meta, proj_meta = subsample_projections_number(
+                photopeak=photopeak_projections, 
+                scatter=scatter_projections, 
+                object_meta=object_meta, 
+                proj_meta=proj_meta,
+                parameters=params["Data_Reduction"]["Projections"]
+            )
+                    
         # Build System Matrix
         
         # Attenuation
